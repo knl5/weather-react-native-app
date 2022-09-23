e30947709f05af14c706340b2aa965563952755c
Kanelle Lorent <kanelle.lorent@mail-ecv.fr>
2022-09-23 15:36:27 +0200

    add search location in progress

--- App.js
+++ App.js
@@ -4,9 +4,11 @@ import { StyleSheet, Text, View, ImageBackground } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import * as Location from 'expo-location';
+import axios from "axios";
 
 import DateTime from './components/DateTime'
 import WeatherScroll from './components/WeatherScroll'
+import SearchBar from './components/SearchLocationWeather'
 const API_KEY ='49cc8c821cd2aff9af04c9f98c36eb74';
 const img = require('./assets/image.png')
 
@@ -49,12 +51,38 @@ export default function App() {
   }
 
   function SearchScreen() {
+  const [searchText,setSearchText] = useState("");
+      const [city,setCity] = useState([]);
+
+     const SearchCities = () =>{
+         axios.get(`http://api.openweathermap.org/data/2.5/weather?q={searchText}&appid=${API_KEY}`)
+             .then( (response) =>{
+                 // handle success
+                 setCity(response.data.city);
+             })
+             .catch(function (error) {
+                 // handle error
+                 console.log(error);
+             })
+             .then(function () {
+                 // always executed
+             });
+     }
     return (
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Text>Search!</Text>
+      <View style={styles.container}>
+       <SearchBar searchText={searchText} setSearchText={setSearchText} onSubmit={SearchCities}/>
+
       </View>
     );
   }
+
+  function ResultScreen() {
+      return (
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+            <SearchCities/>
+        </View>
+      );
+    }
 
   const Tab = createBottomTabNavigator();
 
@@ -62,8 +90,9 @@ export default function App() {
     <View style={styles.container}>
       <NavigationContainer>
             <Tab.Navigator>
-              <Tab.Screen name="Home" component={HomeScreen} />
-              <Tab.Screen name="Search" component={SearchScreen} />
+              <Tab.Screen name="Weather APP - Home" component={HomeScreen} />
+              <Tab.Screen name="Weather APP - Search" component={SearchScreen} />
+              <Tab.Screen name="Weather APP - Result" component={ResultScreen} />
             </Tab.Navigator>
       </NavigationContainer>
     </View>
